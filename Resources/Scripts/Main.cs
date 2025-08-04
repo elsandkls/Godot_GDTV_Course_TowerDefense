70cb590a8898b843456940929d2d3d27c97cd479
ElsAndKls <elsandkls@gmail.com>
2025-08-03 23:25:51 -0400

    init commit
    initial commit, and I finished section 1 of the course.

---
+++ Resources/Scripts/Main.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+
+    public override void _Ready()
+    {
+        base._Ready();
+        GD.Print("Hello World");
+    }
+  
+}
